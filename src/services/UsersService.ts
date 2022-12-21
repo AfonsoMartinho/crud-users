fc2a020eda16db7c4325693b2848a8c8a2acf033
Afonso Martinho <afonso.martinho@namecheap.com>
2022-12-21 00:39:16 +0000

    chore: removed unnecessary code

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -13,15 +13,3 @@ export class UsersService {
         return data.results;
     }
 }
-
-// export const getUsers = async ():Promise<IUser[]> => {
-//     const res = await fetch(baseApiURL);
-//     const data = await res.json();
-//     return await data.results;
-// }
-
-// export const getUsersWithParams = async (params?: string):Promise<IUser[]> => {
-//     const res = await fetch(`${baseApiURL}${params || ''}`);
-//     const data = await res.json();
-//     return await data.results;
-// }
