1c343ff846664f73685c18a73c32712e6e00dfa9
Afonso Martinho <afonso.martinho@namecheap.com>
2023-01-12 16:36:31 +0000

    feat: Added boolean rendering logic to main app page

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,35 @@ import './app.gb.scss';
 
 
 export const App: React.FC = observer((props) => {
-    const { usersStore } = useRootStore();
-    React.useEffect(() => {
-      usersStore.getUsersList();
-    },[])
+	const [isLoading, setIsLoading] = React.useState<boolean>(true);
+	const [errorMessage, setErrorMessage] = React.useState<string>();
+  const { usersStore } = useRootStore();
 
+  const fetchUsersFromStore = async () => {
+		await usersStore.getUsersList()
+		  .then( () => { setIsLoading(false); })
+      .catch( (err)=> {
+        console.log(err);
+        setIsLoading(false);
+        setErrorMessage('No Users Found!')
+      })
+    };
+
+  React.useEffect(() => {
+    if(!isLoading) return;
+    fetchUsersFromStore();
+  },[])
+  
     return (
         <div className="crud-users">
-          {usersStore.usersList && <UsersList usersList={usersStore.usersList} />}
+          { isLoading ? ( <div>isLoading</div> ) : ( 
+          <>
+              { usersStore.usersList.length > 0 &&
+                <UsersList />
+              }
+              { errorMessage!= undefined && ( <div>{errorMessage}</div> )}
+          </> )
+          }
         </div>
     );
   }
