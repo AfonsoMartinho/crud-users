2ee05a93ff04eca5ed8dee8d0a06a5e9b4fdf968
Afonso Martinho <afonso.martinho@namecheap.com>
2023-02-09 17:25:29 +0000

    feat: Updated UsersPage so that it can support the FiltersBar wrapper

--- src/views/UsersPage.tsx
+++ src/views/UsersPage.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { IUser } from "../models/User";
 import { useRootStore } from "../StoreContext";
+import { FiltersBar } from "../wrappers/FiltersBar/indext";
 import { UsersList } from "../wrappers/UsersList";
 
-export const UsersPage: React.FC = () => {
+export const UsersPage: React.FC = ():JSX.Element => {
     const [isLoading, setIsLoading] = React.useState<boolean>(true);
     const [usersList, setUsersList] = React.useState<IUser[]>();
 	const [errorMessage, setErrorMessage] = React.useState<string>();
@@ -11,13 +12,28 @@ export const UsersPage: React.FC = () => {
 
     const fetchUsersFromStore = async () => {
 		await usersStore.getUsersList()
-		    .then( () => { setIsLoading(false); })
+		    .then(() => { 
+                setUsersList(usersStore.usersList)
+                setIsLoading(false);
+            })
             .catch( (err)=> {
             console.log(err);
             setIsLoading(false);
             setErrorMessage('No Users Found!')
         })
     };
+
+    const filterByNationality = (nationality: string) => {
+        usersList?.filter((user) => user.nationality === nationality)
+    };
+
+    const filterByGender = (gender: string) => {
+        usersList?.filter((user) => user.gender === gender)
+    };
+
+    const clearFilters = () => {
+        setUsersList(usersStore.usersList);
+    }
 
     React.useEffect(() => {
         if(!isLoading) return;
@@ -28,10 +44,11 @@ export const UsersPage: React.FC = () => {
         <div className="users-page">
             { isLoading ? ( <div>isLoading</div> ) : ( 
           <>
-              { usersStore.usersList.length > 0 &&
-                <UsersList users={usersStore.usersList} onDeleteUser={(userId: string)=> usersStore.deleteUser(userId)}/>
-              }
-              { errorMessage!= undefined && ( <div>{errorMessage}</div> )}
+            <FiltersBar onClearFilters={clearFilters()} onGenderFilter={(gender: string) => filterByGender(gender)} onNationalityFilter={(nationality: string) => filterByNationality(nationality)}/>
+            { usersList && usersList.length > 0 &&
+                <UsersList users={usersList} onDeleteUser={(userId: string)=> usersStore.deleteUser(userId)}/>
+            }
+            { errorMessage!= undefined && ( <div>{errorMessage}</div> )}
           </> )
           }
         </div>
