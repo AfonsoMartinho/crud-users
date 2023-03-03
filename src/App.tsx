01b8bab32cc39a8e9948bd17a4bfd3ac76f4c3d1
Afonso Martinho <afonso.martinho@namecheap.com>
2023-03-03 17:49:42 +0000

    feat: dark Theme activated by default

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
-import './app.gb.scss';
 import { UsersPage } from './views/UsersPage';
+import './app.gb.scss';
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 
 export const App: React.FC = observer((props) => {
     return (
-        <div className="crud-users">
-          <UsersPage />
-        </div>
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <div className="crud-users">
+            <UsersPage />
+          </div>
+        </ThemeProvider>
     );
   }
 );
